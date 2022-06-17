f46fbb2c82c61373b6cebb226b80fcae373866fc
gianfrancopapa <gpapa@somniosoftware.com>
2022-06-17 18:32:23 -0300

    update post gen script

--- model_json/hooks/post_gen.dart
+++ model_json/hooks/post_gen.dart
@@ -1,14 +1,14 @@
+import 'dart:io';
 import 'package:mason/mason.dart';
 
 void run(HookContext context) async {
-  final completed = context.logger.progress(
+  final progress = context.logger.progress(
     'Creating auto generated models using build_runner',
   );
   await Process.run(
     'flutter',
     ['pub', 'run', 'build_runner', 'build --delete-conflicting-outputs'],
     runInShell: true,
-    workingDirectory: root,
   );
-  completed();
+  progress.complete();
 }
