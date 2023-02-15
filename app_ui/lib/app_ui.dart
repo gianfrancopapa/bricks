bfefe11bf6aa05fff05289d3fa6d28567235a470
carolgsomnio <cglass@somniosoftware.com>
2023-02-15 11:18:03 -0300

    deleted app_ui example

--- app_ui/lib/app_ui.dart
+++
@@ -1,8 +0,0 @@
-library app;
-
-export 'src/colors/app_colors.dart';
-export 'src/icons/app_icons.dart';
-export 'src/spacing/app_spacing.dart';
-export 'src/theme/app_theme.dart';
-export 'src/typography/typography.dart';
-export 'src/widgets/widgets.dart';
