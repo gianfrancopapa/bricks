1737ad2d3e34fecf455a0f03f43eaa9072d74cc5
gianfrancopapa <gpapa@somniosoftware.com>
2023-02-15 19:11:33 -0300

    fix post hook for ui_package

--- ui_package/hooks/post_gen.dart
+++ ui_package/hooks/post_gen.dart
@@ -5,16 +5,23 @@ void run(HookContext context) async {
   final progress = context.logger.progress(
     'Creating auto generated assets using build_runner',
   );
+
+  final directory = Directory.current.path;
+  final vars = context.vars;
+  final packageName = vars['package_name'];
+  final root = "$directory/${packageName}_ui";
   await Process.run(
     'flutter',
     ['pub', 'get'],
     runInShell: true,
+    workingDirectory: root,
   );
-
   await Process.run(
     'flutter',
     ['pub', 'run', 'build_runner', 'build', '--delete-conflicting-outputs'],
     runInShell: true,
+    workingDirectory: root,
   );
+
   progress.complete();
 }
