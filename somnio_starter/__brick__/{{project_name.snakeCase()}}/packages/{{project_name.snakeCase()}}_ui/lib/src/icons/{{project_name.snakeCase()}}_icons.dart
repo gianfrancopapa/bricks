f003d9bffb8978c0fd7722b7d08f8924440f1698
Ana <afernandez@somniosoftware.com>
2024-04-03 16:04:40 -0300

    fix: gallery icons and format

--- somnio_starter/__brick__/{{project_name.snakeCase()}}/packages/{{project_name.snakeCase()}}_ui/lib/src/icons/{{project_name.snakeCase()}}_icons.dart
+++ somnio_starter/__brick__/{{project_name.snakeCase()}}/packages/{{project_name.snakeCase()}}_ui/lib/src/icons/{{project_name.snakeCase()}}_icons.dart
@@ -3,21 +3,23 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:{{project_name.snakeCase()}}_ui/src/generated/assets.gen.dart';
 
 abstract class {{short_name.upperCase()}}Icon {
-    ///Returns the back icon
+  final _package = 'icon_test_ui';
+
+  ///Returns the back icon
   SvgPicture backIcon({double? size, Color? color}) {
     return Assets.icons.backIcon.svg(
-      color: color,
       height: size ?? 14,
       width: size ?? 14,
+      package: _package,
     );
   }
 
   ///Returns the email outline icon
   SvgPicture emailOutline({double? size, Color? color}) {
     return Assets.icons.emailOutline.svg(
-      color: color,
       height: size ?? 14,
       width: size ?? 14,
+      package: _package,
     );
   }
 
