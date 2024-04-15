c2aeeda6e3ccd3f22f86dd3beea8afee924bde24
Ana <98348115+anfernandezz@users.noreply.github.com>
2024-04-15 19:32:19 -0300

    test: add app missing tests, and login (#50)
    * test: add app missing tests, and login
    
    * test: add login tests and navigation observer
    
    * test: add home tests

--- somnio_starter/__brick__/{{project_name.snakeCase()}}/lib/home/bloc/home_state.dart
+++ somnio_starter/__brick__/{{project_name.snakeCase()}}/lib/home/bloc/home_state.dart
@@ -1,10 +1,8 @@
 part of 'home_bloc.dart';
 
-sealed class HomeState extends Equatable {
+class HomeState extends Equatable {
   const HomeState();
-  
+
   @override
   List<Object> get props => [];
 }
-
-final class HomeInitial extends HomeState {}
--- somnio_starter/__brick__/{{project_name.snakeCase()}}/test/authentication/login/view/login_view_test.dart
+++ somnio_starter/__brick__/{{project_name.snakeCase()}}/test/authentication/login/view/login_view_test.dart
@@ -4,14 +4,22 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:form_inputs/form_inputs.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:{{project_name}}/authentication/login/login.dart';
+import 'package:{{project_name}}/keys.dart';
 
 import '../../../helpers/helpers.dart';
 
 void main() {
   late LoginBloc mockLoginBloc;
+  late NavigatorObserver mockNavigatorObserver;
+
+  setUpAll(() {
+    registerFallbackValue(FakeRoute());
+  });
 
   setUp(() {
     mockLoginBloc = MockLoginBloc();
+    mockNavigatorObserver = MockNavigatorObserver();
+
     when(() => mockLoginBloc.state).thenReturn(const LoginState.initial());
   });
 
@@ -92,6 +100,139 @@ void main() {
 
       final signUpButton = find.byType(ElevatedButton);
       expect(tester.widget<ElevatedButton>(signUpButton).enabled, isTrue);
+    });
+
+    testWidgets(
+        'adds [LoginWithEmailAndPasswordRequested]'
+        ' when login button is pressed', (WidgetTester tester) async {
+      when(() => mockLoginBloc.state).thenReturn(
+        const LoginState.initial().copyWith(
+          email: const Email.dirty('email@mail.com'),
+          password: const Password.dirty('Test12345.'),
+        ),
+      );
+
+      await tester.pumpApp(
+        const LoginView(),
+        loginBloc: mockLoginBloc,
+      );
+
+      final loginButton = find.byType(ElevatedButton);
+      await tester.tap(loginButton);
+      verify(
+        () => mockLoginBloc.add(const LoginWithEmailAndPasswordRequested()),
+      ).called(1);
+    });
+
+    testWidgets(
+        'adds [LoginPasswordChanged] when password textfield is changed',
+        (WidgetTester tester) async {
+      await tester.pumpApp(
+        const LoginView(),
+        loginBloc: mockLoginBloc,
+      );
+
+      final passwordTextField = find.byKey(Keys.loginPasswordTextField);
+      await tester.enterText(passwordTextField, 'Test12345.');
+
+      verify(
+        () => mockLoginBloc.add(
+          const LoginPasswordChanged('Test12345.'),
+        ),
+      ).called(1);
+    });
+
+    testWidgets(
+        'changes visibility when [PasswordTextField] icon '
+        ' is tapped', (WidgetTester tester) async {
+      await tester.pumpApp(
+        const LoginView(),
+        loginBloc: mockLoginBloc,
+      );
+
+      final passwordTextField = find.byKey(Keys.loginPasswordTextField);
+
+      await tester.tap(
+        find.descendant(
+          of: passwordTextField,
+          matching: find.byType(IconButton),
+        ),
+      );
+
+      verify(
+        () => mockLoginBloc.add(
+          const LoginPasswordVisibilityChanged(obscure: false),
+        ),
+      ).called(1);
+    });
+
+    testWidgets('[EmailTextField] should display error state',
+        (WidgetTester tester) async {
+      when(() => mockLoginBloc.state).thenReturn(
+        const LoginState.initial().copyWith(
+          email: const Email.dirty('asd'),
+        ),
+      );
+
+      await tester.pumpApp(
+        const LoginView(),
+        loginBloc: mockLoginBloc,
+      );
+
+      final emailTextField = find.byKey(Keys.loginEmailTextField);
+      expect(
+        tester.widget<TextField>(emailTextField).decoration?.errorText,
+        isNotNull,
+      );
+    });
+
+    testWidgets('[EmailTextField] should add [LoginEmailChanged] event',
+        (WidgetTester tester) async {
+      when(() => mockLoginBloc.state).thenReturn(
+        const LoginState.initial().copyWith(
+          email: const Email.dirty('asd'),
+        ),
+      );
+
+      await tester.pumpApp(
+        const LoginView(),
+        loginBloc: mockLoginBloc,
+      );
+
+      final emailTextField = find.byKey(Keys.loginEmailTextField);
+      await tester.enterText(emailTextField, 'email');
+
+      verify(
+        () => mockLoginBloc.add(
+          const LoginEmailChanged('email'),
+        ),
+      ).called(1);
+    });
+
+    testWidgets('[ForgotPasswordButton] navigates on pressed',
+        (WidgetTester tester) async {
+      await tester.pumpApp(
+        const LoginView(),
+        loginBloc: mockLoginBloc,
+        navigatorObserver: mockNavigatorObserver,
+      );
+
+      final forgotPasswordButton = find.byKey(Keys.loginForgotPasswordButton);
+      await tester.tap(forgotPasswordButton);
+      verify(() => mockNavigatorObserver.didPush(any(), any()));
+    });
+
+    testWidgets('[SignUpButton] navigates on pressed',
+        (WidgetTester tester) async {
+      await tester.pumpApp(
+        const LoginView(),
+        loginBloc: mockLoginBloc,
+        navigatorObserver: mockNavigatorObserver,
+      );
+
+      final signUpButton = find.byKey(Keys.loginSignUpButton);
+      await tester.tap(signUpButton);
+      verify(() => mockNavigatorObserver.didPush(any(), any()));
     });
   });
 }
