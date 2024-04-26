1d24ca3c646860641a6fe30f7e7cfe0d2d759a71
MarcosTort <tort.marcos9@gmail.com>
2024-04-26 14:27:28 -0300

    Add tests for DownForMaintenancePage and DownForMaintenanceView

---
+++ somnio_starter/__brick__/{{project_name.snakeCase()}}/test/force_upgrade/force_upgrade_view_test.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:{{project_name}}/force_upgrade/force_upgrade.dart';
+import 'package:{{project_name}}/l10n/l10n.dart';
+
+void main() {
+  group('ForceUpgradeView test', () {
+    testWidgets('renders correct text', (WidgetTester tester) async {
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: ForceUpgradeView(),
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+          supportedLocales: AppLocalizations.supportedLocales,
+        ),
+      );
+
+      expect(find.byType(Text), findsOneWidget);
+    });
+
+    testWidgets('showAsDialog displays dialog', (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Builder(
+            builder: (BuildContext context) {
+              return ElevatedButton(
+                onPressed: () {
+                  ForceUpgradeView.showAsDialog(context);
+                },
+                child: const Text('Show Dialog'),
+              );
+            },
+          ),
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+          supportedLocales: AppLocalizations.supportedLocales,
+        ),
+      );
+
+      await tester.tap(find.text('Show Dialog'));
+      await tester.pumpAndSettle();
+
+      expect(find.byType(Dialog), findsOneWidget);
+    });
+  });
+}
